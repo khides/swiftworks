24088e83a993676e8fc70ca9ba2664a768230638
hideshima <taikoud29@gmail.com>
2023-10-12 11:39:22 +0900

    test

---
+++ Test/Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by 秀島光樹 on 2023/10/12.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
